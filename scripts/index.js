1e54fd814b421f99a78de6a0ce35d9eadf6071a3
JulioEVA <julio-emmanuel2011@hotmail.com>
2023-07-04 22:07:24 -0700

    Refactor: Create the utils.js script for better separation of code into modules.

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,18 @@
 import { Card } from "./Card.js";
 import { FormValidator } from "./FormValidator.js";
-const btnEdit = document.querySelector(".edit-button");
-const btnClose = document.querySelector(".close-button");
-const btnAdd = document.querySelector(".add-button");
-const btnCloseImagePopup = document.querySelectorAll(".close-button")[1];
-const imagePopup = document.querySelector(".image-popup");
-const modal = document.querySelector(".edit-popup");
-const modalButton = document.querySelector(".save-button");
+const firstResult = 0;
+const secondResult = 1;
+const elements = document.querySelector(".elements");
+const formElement = document.querySelector("form");
 const modalTitle = document.querySelector(".edit-popup__title");
 const profileName = document.querySelector(".profile__title");
-const formElement = document.querySelector("form");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 const inputs = document.querySelectorAll(".input");
-const firstInput = 0;
-const secondInput = 1;
-const elements = document.querySelector(".elements");
-const nameInput = inputs[firstInput];
-const aboutInput = inputs[secondInput];
+const nameInput = inputs[firstResult];
+const aboutInput = inputs[secondResult];
 const elementTitle = nameInput;
 const imageLink = aboutInput;
+const modal = document.querySelector(".edit-popup");
 const initialCards = [
   {
     name: "Valle de Yosemite",
@@ -45,6 +39,7 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
   },
 ];
+
 /**
  * Load elements on page startup.
  */
@@ -59,68 +54,6 @@ initialCards.forEach((item) => loadElement(item.name, item.link));
 function loadElement(elementTitle, imageLink) {
   const newCard = new Card(elementTitle, imageLink, ".element-template");
   elements.prepend(newCard.createCardElement());
-}
-
-/**
- * Opens edit modal
- */
-function openModalEdit(evt) {
-  modalTitle.textContent = "Editar perfil";
-  modalButton.textContent = "Guardar";
-  nameInput.placeholder = "Nombre";
-  aboutInput.placeholder = "Acerca de mí";
-  aboutInput.maxlength = "200";
-  aboutInput.type = "text";
-  nameInput.value = profileName.textContent;
-  aboutInput.value = profileSubtitle.textContent;
-  modal.showModal();
-}
-/**
- * Closes the open popup with a smooth transition.
- * @param {*} evt The closing event.
- * @param {*} popup The popup itself.
- */
-function closePopup(evt, popup) {
-  popup.setAttribute("closing", true);
-  popup.addEventListener(
-    "animationend",
-    () => {
-      popup.removeAttribute("closing");
-      popup.close();
-    },
-    { once: true }
-  );
-  evt.preventDefault;
-}
-
-/**
- * Closes de edit modal.
- */
-function closeModal(evt) {
-  closePopup(evt, modal);
-}
-
-/**
- * Closes the open image.
- */
-function closeImagePopup(evt) {
-  closePopup(evt, imagePopup);
-}
-
-/**
- * Opens the modal to add a new element.
- * @param {*} evt The form submit element.
- */
-function openAddModal(evt) {
-  modalTitle.textContent = "Nuevo lugar";
-  modalButton.textContent = "Crear";
-  elementTitle.placeholder = "Título";
-  imageLink.placeholder = "Enlace a la imagen";
-  nameInput.value = "";
-  aboutInput.value = "";
-  aboutInput.removeAttribute("maxlength");
-  aboutInput.type = "url";
-  modal.showModal();
 }
 
 /**
@@ -152,12 +85,6 @@ function handleElementFormSubmit(evt) {
   modal.close();
 }
 
-function closeOnOutsideClick(evt) {
-  if (evt.target === modal || evt.target === imagePopup) {
-    closePopup(evt, evt.target);
-  }
-}
-
 const formValidator = new FormValidator(
   {
     formSelector: ".form",
@@ -171,13 +98,17 @@ const formValidator = new FormValidator(
 );
 formValidator.enableValidation();
 
-modal.addEventListener("click", closeOnOutsideClick);
-imagePopup.addEventListener("click", closeOnOutsideClick);
-
 formElement.addEventListener("submit", handleProfileFormSubmit);
 formElement.addEventListener("submit", handleElementFormSubmit);
 
-btnCloseImagePopup.addEventListener("click", closeImagePopup);
-btnAdd.addEventListener("click", openAddModal);
-btnEdit.addEventListener("click", openModalEdit);
-btnClose.addEventListener("click", closeModal);
+export {
+  modalTitle,
+  profileName,
+  profileSubtitle,
+  nameInput,
+  aboutInput,
+  elementTitle,
+  imageLink,
+  secondResult,
+  modal,
+};
